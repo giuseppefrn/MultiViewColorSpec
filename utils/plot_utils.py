9cffc42fbe9ef694aa91686b4439e6f6d434dce3
ozy <giuseppe.fur96@gmail.com>
2022-10-17 16:44:27 +0200

    plot label changes

--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -37,25 +37,25 @@ def plot_loss(history, output_dir):
 
 def plot_de(history, output_dir):
     plt.figure(figsize=(16,12))
-    plt.plot(history['epoch'], history['validation_DE'], label='val_delta_e', color='orange')
-    plt.plot(history['epoch'], history['test_DE'], label='test_delta_e', color='red')
-    plt.plot(history['epoch'], history['DE'], label='delta_e', color='blue')
+    plt.plot(history['epoch'], history['validation_DE'], label='valid $\Delta$E', color='orange')
+    plt.plot(history['epoch'], history['test_DE'], label='test $\Delta$E', color='red')
+    plt.plot(history['epoch'], history['DE'], label='train $\Delta$E2', color='blue')
     plt.legend()
-    plt.title('DELTA E')
+    plt.title('CIEDE2000')
     check_dir(output_dir)
     plt.savefig(os.path.join(output_dir,'imgs' ,'de.png'))
 
 def plot_hist_pred_delta_e(arr, split_name, output_dir, n_bins=100):
     plt.figure(figsize=(16,12))
     plt.hist(arr,bins=n_bins)
-    plt.title(split_name + ' Delta E')
+    plt.title(split_name + ' $\Delta$E2000')
     check_dir(output_dir)
     plt.savefig(os.path.join(output_dir, 'imgs' ,'{}-hist.png'.format(split_name)))
 
 def plot_boxplot_pred_delta_e(arr, output_dir):
     plt.figure(figsize=(16,12))
     plt.boxplot(arr, labels=['Train', 'Valid', 'Test'])
-    plt.title('Delta E')
+    plt.title('$\Delta$E2000')
     check_dir(output_dir)
     plt.savefig(os.path.join(output_dir,'imgs','box-plot.png'))
 
